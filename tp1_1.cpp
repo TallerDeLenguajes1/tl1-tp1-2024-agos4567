b60364be09092fd0b6f138032b8e7d7fde1c3536
agos4567 <agosgalfre23@gmail.com>
2024-03-28 00:33:59 -0300

    punto3

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -6,6 +6,22 @@ int main(){
 
 printf ("hola mundo\n");
 
+//punto 3e
+
+int *p;
+
+int a;
+
+p=&a;
+
+printf("ingrese un numero :\n");
+scanf("%d", &a);
+
+
+printf("el contenido apuntado de p: %d\n", *p);
+printf("direccion de memoria es del puntero: %d\n", &p);
+printf("direccion de memoria de la variable es %d :\n", &a);
+printf("el tamanio de memoria utilizado por la variable: %d\n", sizeof(a));
 
 
 
